823ab4920d709ef029712836122200ee7458a2db
dungthinhtv <102164139+dungthinhtv@users.noreply.github.com>
2022-09-14 12:56:19 +0700

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,26 @@
+import { useState } from 'react';
+import reactLogo from './assets/react.svg';
+
+import Header from './components/Header';
+import AuthButtons from './components/AuthButtons';
+import Nav from './components/Nav/index';
+
+import './App.css';
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div className="grid md:grid-cols-5">
+      <Nav />
+      <main className="px-12 py-6 md:col-span-4 bg-cyan-50">
+        {/* <h3 className="text-3xl">Hello</h3> */}
+        <AuthButtons />
+        <Header />
+        {/* <Trending /> */}
+      </main>
+    </div>
+  );
+}
+
+export default App;
